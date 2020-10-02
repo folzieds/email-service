220df3de3ddbbe9107b8796b02db7a9a5ea90f70
Adefolarin Osu <folzieds@gmail.com>
2020-10-02 18:59:27 +0100

    Added Id for table creation and properties for mysql

--- src/main/java/com/phos/email/models/EmailModel.java
+++ src/main/java/com/phos/email/models/EmailModel.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 public class EmailModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
